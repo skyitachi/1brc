ce6aaaa820492e05468543767b5f036efaa382de
yaosp <yaosp@trip.com>
2025-03-11 21:02:52 +0800

    sync code

--- src/main/java/dev/morling/onebrc/CalculateAverage_skyitachi.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_skyitachi.java
@@ -23,43 +23,67 @@ public class CalculateAverage_skyitachi {
 
     private static Map<String, double[]> stats = new HashMap<>();
 
-    private static byte[] left = new byte[4096];
+    private static byte[] parseBuffer = new byte[4096];
+    private static String currentKey = "";
+    private static double currentValue = 0;
+    private static int currentOffset = 0;
+    private static boolean isInKey = true;
 
     public static void main(String []args) throws IOException {
         try (RandomAccessFile raf = new RandomAccessFile(String.valueOf(FILE), "r")) {
             FileChannel channel = raf.getChannel();
             ByteBuffer buffer = ByteBuffer.allocate(4096);
             channel.read(buffer);
+            parseBlock(buffer);
+        }
+        for(Map.Entry<String, double[]> entry : stats.entrySet()) {
+            double[] values = entry.getValue();
+            double avg = values[0] / values[1];
+            System.out.println(entry.getKey() + ": " + avg + ", max: " + values[2] + ", min: " + values[3]);
         }
     }
 
     /**
-     * @param buffer 需要解析的buffer，不包含left buffer
-     * @param status, 0: 不需要parse left buffer，1：需要parse，且没解析完key，2: 需要parse，需要解析value, 3: left buffer已经parse完成
-     * @param leftover, left buffer里的总长度
-     * @param offset, value在left buffer里的offset
      */
-    public static void parseBlock(ByteBuffer buffer, int status, int leftover, int offset) {
+    public static void parseBlock(ByteBuffer buffer) {
         buffer.flip();
-        boolean isInKey = true;
-        if (status == 2) {
-            isInKey = false;
-        }
-        int currentKeyLen = 0;
-        if (status != 0) {
-            currentKeyLen = offset;
-        }
-        int currentValueOffset = 0;
         while (buffer.hasRemaining()) {
             byte b = buffer.get();
             if (b == NEW_LINE) {
                 // 判断是否要parse left buffer里的数据
-                if (status == 0) {
-                    // 解析value
+                String valueString = new String(parseBuffer, 0, currentOffset);
+                currentValue = Double.parseDouble(valueString);
+                if (stats.containsKey(currentKey)) {
+                    double[] values = stats.get(currentKey);
+                    values[0] += currentValue;
+                    values[1] += 1;
+                    if (currentValue > values[2]) {
+                        values[2] = currentValue;
+                    }
+                    if (currentValue < values[3]) {
+                        values[3] = currentValue;
+                    }
+                } else {
+                    double[] values = new double[4];
+                    values[0] = currentValue;
+                    values[1] = 1;
+                    values[2] = currentValue;
+                    values[3] = currentValue;
+                    stats.put(currentKey, values);
                 }
-                continue;
+                isInKey = true;
+            } else if (b == COLON) {
+                if (isInKey) {
+                  currentKey = new String(parseBuffer, 0, currentOffset);
+                  isInKey = false;
+                  currentOffset = 0;
+                }
+            } else {
+                parseBuffer[currentOffset] = b;
+                currentOffset++;
             }
         }
+        buffer.flip();
     }
 
 
